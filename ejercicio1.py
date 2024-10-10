4f73f8ff77a5cf3da2f23a288c1280be5489db8e
Esme <esmeralda.lunaromano14@mail.com>
2024-10-10 09:57:49 -0600

    Primer ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+"""
+Ejercicio 1:
+    Escribir un programa que pegunte al usuario su nombree e imprima un saluddo
+Entradas
+    nombre: String 
+Salida:
+    saludo: string
+"""""
+nombre = input( "Ecribe tu nombre: " )
+edad = input("Escribe tu edad: /n")
+saludo = "Hola " + nombre + " de " + edad + " años "  " !"
+print(saludo)
