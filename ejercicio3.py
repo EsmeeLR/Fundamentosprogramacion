bc76e47e61d2832e83ece15197f5e0f18cc965df
Esme <esmeralda.lunaromano14@mail.com>
2024-10-14 09:52:01 -0600

    03 ejercicio3

---
+++ ejercicio3.py
@@ -0,0 +1,18 @@
+""" Ejercicio 3:
+    Programa que calcula la hipotenusa de un triangulo rectangulo a partir de sus catetos
+Entradas:
+    cateto1: int
+    cateto2: int
+Salidas:
+    hipotenusa
+Análisis:
+    Se resuelve con el teorema de pitagoras
+"""
+import math
+cateto1 = input ("Escribe el cateto 1: ")
+cateto1 = int(cateto1)
+cateto2 = int (input("Escribe el cateto 2:"))
+hipotenusa = cateto1 * cateto1 * cateto2 * cateto2
+hipotenusa = hipotenusa ** (1/2)
+hipotenusa = math.sqrt(hipotenusa)
+print("La hipotenusa es: " hipotenusa)
