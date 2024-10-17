7d12f62b47de43745505f3703833efa4d61480d4
Esme <esmeralda.lunaromano14@mail.com>
2024-10-17 09:41:19 -0600

    ejercicio3

--- ejercicio3.py
+++ ejercicio3.py
@@ -15,4 +15,4 @@ cateto2 = int (input("Escribe el cateto 2:"))
 hipotenusa = cateto1 * cateto1 * cateto2 * cateto2
 hipotenusa = hipotenusa ** (1/2)
 hipotenusa = math.sqrt(hipotenusa)
-print("La hipotenusa es: " hipotenusa)
+print ("La hipotenusa es: ", hipotenusa)
