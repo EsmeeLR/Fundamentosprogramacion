19f02c47c636975e01fdee3776df09f54128453b
Esme <esmeralda.lunaromano14@mail.com>
2024-10-18 12:30:09 -0600

    ejercicio 06

---
+++ ejercicio6.py
@@ -0,0 +1,11 @@
+"""
+Programa que calcula la media de tres números pedidos por el teclado
+"""
+print("Calcula la media de tres numeros")
+
+num1 = int(input ("Ingresa el número 1: "))
+num2 = int(input ("Ingresa el número 2: "))
+num3 = int(input ("Ingresa el número 3: "))
+media = (num1 + num2 + num3)/3
+
+print ("La  media de los tres nímeros ingresados es:", media)
