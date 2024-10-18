847e6dcfa20a83a35c72ed6a729e0dde3b0cc42f
Esme <esmeralda.lunaromano14@mail.com>
2024-10-18 12:50:04 -0600

    ejercicio 06

---
+++ ejercicio7.py
@@ -0,0 +1,10 @@
+"""
+Programa que reciba una cantidad de minutos y muestre por pantalla a cuantas horas y minutos corresponde
+"""
+print ("Convierte una cantidad de minutos a horas")
+
+minutos = int(input("Ingrese la cantidad en  minutos: "))
+horas = minutos // 60
+minres = minutos % 60
+print ("Los minutos equivalen a:", horas,"horas y", minres, "minutos.")
+
