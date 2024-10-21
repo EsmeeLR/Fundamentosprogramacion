39c0cc82f4d6fe4b46142758ea61fb9ab00df8b1
Esme <esmeralda.lunaromano14@mail.com>
2024-10-21 09:46:34 -0600

    Ejercicio 11

---
+++ ejercicio11.py
@@ -0,0 +1,7 @@
+"""
+Programa que pide al usuario dos números y muestra la distancia entre ellos (el valor absoluto de su diferencia)
+"""
+a = int(input("Ingresa el valor de la primera cantidad: "))
+b = int(input("Ingresa el valor de la segunda cantidad: "))
+distancia = abs(a - b)
+print("La distancia entre los números dados es:",distancia) 
