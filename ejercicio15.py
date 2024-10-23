1b050aca45f4ddaaacda82e4f660a7afa6c9cc1e
Esme <esmeralda.lunaromano14@mail.com>
2024-10-23 10:35:17 -0600

    ejercicio 15

--- ejercicio15.py
+++ ejercicio15.py
@@ -1,17 +1,14 @@
 """
- Dadas dos variables numéricas A y B, que el usuario debe teclear, se pide
+ Dadas dos variables numéricas A y B,
 realizar un programa que intercambie los valores de ambas variables y muestre
 cuanto valen al final las dos variables.   
 """
-# Solicitar al usuario que introduzca los valores de A y B
+
 A = float(input("Introduce el valor de A: "))
 B = float(input("Introduce el valor de B: "))
 
-# Mostrar los valores originales
 print("Valores originales  A:", A, "B:", B)
-# Intercambiar los valores
 temp = A
 A = B
 B = temp
-# Mostrar los nuevos valores
 print("Valores intercambiados  A:", A, "B:", B)
